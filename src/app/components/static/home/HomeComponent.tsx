195800e551869d05f73c28821f832ce47cb48af3
Miguel A. Gómez <>
2023-07-17 18:46:47 +0100

    improve design

--- src/app/components/static/home/HomeComponent.tsx
+++ src/app/components/static/home/HomeComponent.tsx
@@ -1,11 +1,13 @@
 import {Newsletter} from "@/app/components/client/newsletter/newsletter";
 import {ListOfPosts} from "@/app/components/static/listOfPosts/listOfPosts";
+import styles from './home.module.css';
 
 export const HomeComponent = ({params}: { params: { slug: string }; }) => {
     const parsedSlug = Number(params.slug);
     const currentPage = parsedSlug ? parsedSlug : 1;
     return (
         <>
+            <h1 className={styles.title}>Cansados de escribir código que mata la moral de cualquiera</h1>
             <Newsletter/>
             <ListOfPosts title={"Últimos artículos"} currentPage={currentPage} />
         </>
