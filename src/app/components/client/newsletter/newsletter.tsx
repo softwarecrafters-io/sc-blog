8a3e70e44eddf67059bff58726105931265b4c8c
Miguel A. Gómez <>
2023-07-19 22:25:00 +0100

    fix mobile

--- src/app/components/client/newsletter/newsletter.tsx
+++ src/app/components/client/newsletter/newsletter.tsx
@@ -14,14 +14,14 @@ export const Newsletter = ({hideEntry = false}:{hideEntry?:boolean}) => {
     return <div className={styles.container} id={'newsletter'}>
         <div className={styles.header}>
             <h3 className={styles.title}>La newsletter, por Miguel A. Gómez</h3>
-            <Image hidden={hideEntry} className={styles.profilePhone} src={'https://swcrafters.fra1.cdn.digitaloceanspaces.com/Assets/foto-circle-small.png'} alt={'Miguel A. Gómez - Clean JavaScript'} width={140} height={140}/>
+            {hideEntry ? <></> : <Image  className={styles.profilePhone} src={'https://swcrafters.fra1.cdn.digitaloceanspaces.com/Assets/foto-circle-small.png'} alt={'Miguel A. Gómez - Clean JavaScript'} width={140} height={140}/>}
             <div className={styles.content}>
                 <div className={styles.textContainer}>
-                    <p hidden={hideEntry} className={styles.text}>Cada lunes, miércoles y viernes escribo un email con <strong>un consejo para subir de nivel como developer</strong>. Semana que estás fuera, consejos que te pierdes.</p>
-                    <p hidden={hideEntry} className={styles.text}>Aprenderás a escribir mejor código gracias a arquitectos en torres de marfil, barcos que se hunden y a developers que desarrollan lenguajes en 10 días ... </p>
+                    {hideEntry ? <></> : <p className={styles.text}>Cada lunes, miércoles y viernes escribo un email con <strong>un consejo para subir de nivel como developer</strong>. Semana que estás fuera, consejos que te pierdes.</p>}
+                    {hideEntry ? <></> : <p className={styles.text}>Aprenderás a escribir mejor código gracias a arquitectos en torres de marfil, barcos que se hunden y a developers que desarrollan lenguajes en 10 días ... </p>}
                     <p className={styles.text}>Únete a las miles de personas que han descubierto una forma diferente de entender la programación.</p>
                 </div>
-                <Image hidden={hideEntry} className={styles.profile} src={'https://swcrafters.fra1.cdn.digitaloceanspaces.com/Assets/foto-circle-small.png'} alt={'Miguel A. Gómez - Clean JavaScript'} width={140} height={140}/>
+                {hideEntry ? <></> :<Image hidden={hideEntry} className={styles.profile} src={'https://swcrafters.fra1.cdn.digitaloceanspaces.com/Assets/foto-circle-small.png'} alt={'Miguel A. Gómez - Clean JavaScript'} width={140} height={140}/>}
             </div>
         </div>
         <div className={styles.formContainer}>
