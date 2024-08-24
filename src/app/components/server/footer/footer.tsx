c5d1c37878057c44e6a793b3257350ca367f76ab
Miguel A. Gómez <software.crafters.sl@gmail.com>
2024-08-24 18:31:11 +0100

    feat: change footer

--- src/app/components/server/footer/footer.tsx
+++ src/app/components/server/footer/footer.tsx
@@ -6,7 +6,7 @@ export const Footer = () => {
     const year = new Date().getFullYear();
     return (
         <div className={styles.container}>
-            <Link className={styles.legal} href={Routes.legal}>Legal y Política de Privacidad</Link>
+            <Link className={styles.legal} href={Routes.legal}>Aviso Legal</Link>
             <p className={styles.cookies}>® Software Crafters es una marca registrada.</p>
         </div>
     )
