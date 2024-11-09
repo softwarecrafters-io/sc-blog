ab76883f71bfdf1488f5e391c45d7ece3a360d56
Miguel A. Gómez <software.crafters.sl@gmail.com>
2024-11-09 14:08:06 +0000

    feat: improve footer

--- src/app/components/server/footer/footer.tsx
+++ src/app/components/server/footer/footer.tsx
@@ -6,7 +6,7 @@ export const Footer = () => {
     const year = new Date().getFullYear();
     return (
         <div className={styles.container}>
-            <p className={styles.cookies}>Software Crafters ®  | Creado con 🖤 para elevar el nivel de la conversación sobre programación en español 💪🏼 | <Link className={styles.legal} href={Routes.legal}>Legal</Link></p>
+            <p className={styles.cookies}>Software Crafters ®  | Creado con 🖤 para elevar el nivel de la conversación sobre programación en español | <Link className={styles.legal} href={Routes.legal}>Legal</Link></p>
         </div>
     )
 }
