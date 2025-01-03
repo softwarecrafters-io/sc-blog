dfcf801ccef56321f1230b3f7b36b4a39276f880
Miguel A. Gómez <software.crafters.sl@gmail.com>
2025-01-03 18:05:56 +0000

    fix get offer

--- src/app/(no-header)/curso-principios/importante-mensaje/page.tsx
+++ src/app/(no-header)/curso-principios/importante-mensaje/page.tsx
@@ -3,9 +3,12 @@ import {OfferMessage} from "@/app/(no-header)/curso-principios/importante-mensaj
 
 async function getOffer() {
     const headersList = headers();
-    const ip = headersList.get('x-forwarded-for') || headersList.get('x-real-ip')  || 'unknown';
-    console.log('IP:', ip);
-    let url = `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3010'}/api/offers?identifier=clean-js-offer`;
+    const ip = headersList.get('x-forwarded-for') || 'unknown';
+    const host = headersList.get('host') || '';
+    const baseUrl = host.includes('localhost')
+        ? 'http://localhost:3010'
+        : 'https://softwarecrafters.io';
+    const url = `${baseUrl}/api/offers?identifier=clean-js-offer`;
     const response = await fetch(url, {
         headers: {'X-Forwarded-For': ip},
         cache: 'no-store'
